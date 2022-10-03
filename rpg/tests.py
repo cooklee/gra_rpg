3806cc05bc83da606dfdd5b3a082f5358a681895
cooklee <sbbogus@gmail.com>
2022-10-03 16:57:34 +0200

    add hero view + testy

--- rpg/tests.py
+++ rpg/tests.py
@@ -1,9 +1,35 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
+
+from rpg.forms import HeroCreateForm
+from rpg.models import Hero
 
 
 def test_index():
-    clinet = Client()
+    client = Client()
     url = '/'
-    response = clinet.get(url)
+    response = client.get(url)
     assert response.status_code == 200
     assert 'gra rpg' in str(response.content)
+
+
+def test_add_hero_get():
+    client = Client() # otwórz przeglądarke
+    url = reverse('create_hero') # znajdz url o nazwie create_hero
+    response = client.get(url) # wejdz metodą get nasza symulacyjną przegladarka na tego URL
+    assert response.status_code == 200
+    form_in_view = response.context['formularz'] # pobierz z kontekstu zmienną o nazwie formularz
+    assert isinstance(form_in_view, HeroCreateForm)
+
+@pytest.mark.django_db # ten dekorator mowi temu testowi wolno dotykać bazy danych
+def test_add_hero_post():
+    client = Client() # otwórz przeglądarke
+    url = reverse('create_hero') # znajdz url o nazwie create_hero
+    data = {     # wpisz w formularzy w pole name wartość slawek
+        'name':'slawek',
+    }
+    response = client.post(url, data) # wejdz metodą post nasza symulacyjną przegladarka na tego URL
+    assert response.status_code == 302 # bo w widoku jest redirect
+    assert Hero.objects.get(name='slawek') # sprawdz czy slawek jest w bazie danych
+
--- rpg/views.py
+++ rpg/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 from rpg.forms import HeroCreateForm
+from rpg.models import Hero
 
 
 # Create your views here.
@@ -17,3 +18,13 @@ class CreateHeroView(View):
     def get(self, request):
         form = HeroCreateForm()
         return render(request, 'create_hero.html', {'formularz':form})
+
+    def post(self, request):
+        form = HeroCreateForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            Hero.objects.create(name=name)
+            return redirect('create_hero')
+        return render(request, 'create_hero.html', {'formularz': form})
+
